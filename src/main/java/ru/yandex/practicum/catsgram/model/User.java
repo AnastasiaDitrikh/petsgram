23a5b08d01332a902a90c8a4c483f194b1458f82
Nastya <nastya.pgn081@gmail.com>
2024-01-28 11:00:25 +0300

    Create User model

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class User {
+
+    private String email;
+    private String nickname;
+    private LocalDate birthdate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
